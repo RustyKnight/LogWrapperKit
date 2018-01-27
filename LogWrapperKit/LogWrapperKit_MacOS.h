b5866d99570092f4f96a25a4dec3ad8d023bd604
Shane Whitehead <obishiro@gmail.com>
2018-01-27 13:47:26 +1100

    Fix MacOS product name

---
+++ LogWrapperKit/LogWrapperKit_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  LogWrapperKit_MacOS.h
+//  LogWrapperKit-MacOS
+//
+//  Created by Shane Whitehead on 27/1/18.
+//  Copyright © 2018 Shane Whitehead. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LogWrapperKit_MacOS.
+FOUNDATION_EXPORT double LogWrapperKit_MacOSVersionNumber;
+
+//! Project version string for LogWrapperKit_MacOS.
+FOUNDATION_EXPORT const unsigned char LogWrapperKit_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogWrapperKit_MacOS/PublicHeader.h>
+
+
